f05a9e9161616ad513ba11d167f4c077f46dc345
Panela Leung <leungpanela@gmail.com>
2019-08-27 22:07:34 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,13 +15,13 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-  contacts.each do |person,contact_details_hash|
+contacts.each do |person,contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute,data|
       if attribute == "favorite_ice_cream_flavors"
-        
-      binding.pry
+        binding.pry
+      end
     end
   end
-  end
+end
 end
